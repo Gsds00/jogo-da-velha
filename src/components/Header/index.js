69a2e0d62884149eb282405dcd438975a9ac6694
Gabriel Severino <gabrielseverinooo000@gmail.com>
2023-04-11 23:04:38 -0300

    flex-box no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import './styles.css';
 import Logo from '../../objects/Logo';
 import Sobre from '../../objects/Sobre';
 import Menu from '../../objects/menu';
+
 
 const Header = () => (
     <header className="header">
